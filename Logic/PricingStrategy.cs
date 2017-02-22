34020ff5a10779dcddb4ecd1808080f0469aade3
sander541 <sandersven1@Gmail.com>
2017-02-22 13:46:00 +0200

    PricingStrategyTestid

--- Logic/PricingStrategy.cs
+++ Logic/PricingStrategy.cs
@@ -2,7 +2,10 @@
 {
     public enum PricingStrategy
     {
+        NotKnown = 0,
         Aggregated = 1,
-        Assigned = 2
+        Assigned = 2,
+        NotApplicable = 9
+
     }
 }
--- Tests/Logic/PricingStrartegyTests.cs
+++ Tests/Logic/PricingStrartegyTests.cs
@@ -1,6 +1,40 @@
-﻿namespace Tests.Logic
+﻿using System;
+using Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Logic
 {
-    class PricingStrartegyTests
+    [TestClass]
+    public class PricingStrartegyTests
     {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Assert.AreEqual(4, Enum.GetNames(typeof(PricingStrategy)).Length);
+        }
+
+        [TestMethod]
+        public void NotKnownTest()
+        {
+            Assert.AreEqual(0, (int)PricingStrategy.NotKnown);
+        }
+
+        [TestMethod]
+        public void AggregatedTest()
+        {
+            Assert.AreEqual(1, (int) PricingStrategy.Aggregated);
+        }
+
+        [TestMethod]
+        public void AssignedTest()
+        {
+            Assert.AreEqual(2, (int) PricingStrategy.Assigned);
+        }
+
+        [TestMethod]
+        public void NotApplicableTest()
+        {
+            Assert.AreEqual(9, (int)PricingStrategy.NotApplicable);
+        }
     }
 }
