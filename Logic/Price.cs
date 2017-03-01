b3b6e02108c57c096346bc095e4fc962b59906df
sander541 <sandersven1@Gmail.com>
2017-03-01 18:07:25 +0200

    PriceClass

--- Logic/Price.cs
+++ Logic/Price.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Logic
+﻿namespace Logic
 {
-    public class Price
+    public class Price : Money
+    {
+        public Money Money { get; set; }
+        public Price(Money money)
+        {
+            this.Money = money;
+        }
+
+    }
+
+    public class Money
     {
     }
 }
