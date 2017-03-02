ac8638b58934ea8d2ab4108d789ff263dced4633
erihvalter <ErihValter-PC@DESKTOP-S8KIO63>
2017-03-02 11:27:39 +0200

    PackageTypeTests

--- Tests/Logic/PackageTypeTests.cs
+++ Tests/Logic/PackageTypeTests.cs
@@ -1,6 +1,22 @@
-﻿namespace Tests.Logic
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Logic
 {
-    class PackageTypeTests
-    {
+    [TestClass]
+    public class PackageTypeTests
+    {  
+
+        [TestMethod]
+        public void PackageTypeTest()
+        {
+            PackageType packageType = new PackageType(PricingStrategy.Aggregated);
+
+            string expected = "Aggregated";
+            string actual = packageType.PricingStrategy.ToString();
+            Assert.AreEqual(expected, actual);
+        }
     }
 }
