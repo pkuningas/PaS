8b4ec4110f56546ec2ed7bf2f9c2d32ff6d274ba
pkuningas <pille.kuningas@gmail.com>
2017-03-16 13:06:03 +0200

    ProductTypeTests ConstructorTest ja veidi InitializeTesti

--- Tests/Logic/ProductTypeTests.cs
+++ Tests/Logic/ProductTypeTests.cs
@@ -1,6 +1,31 @@
-﻿namespace Tests.Logic
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Logic;
+
+namespace Tests.Logic
 {
-    class ProductTypeTests
+    public class ProductTypeTests
     {
+        private ProductType productType;
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+           // productType = new ProductType(); sulgudesse vaja koodi lisada
+
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+
+        }
+
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            string ID = "Name";
+            ProductType target = new ProductType(ID);
+            Assert.AreEqual<string>(ID, target.ID, "Name");
+        }
     }
 }
